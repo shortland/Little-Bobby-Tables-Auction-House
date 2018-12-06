995fb1b7a9b0a4df34ea71971de361c44a853b74
Josephine <Josephine@10.1.177.206>
2018-12-06 15:23:36 -0500

    Edit login()-David

--- dao/LoginDao.java
+++ dao/LoginDao.java
@@ -19,21 +19,26 @@ public class LoginDao {
 		 * Query to verify the username and password and fetch the role of the user, must be implemented
 		 */
 		String authorizationLevel = "";
+		String password="";
+		String email="";
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://138.197.50.244:3306/LittleBobbyTablesAuctionHouse",  "littlebobbytables", "bestcse305group");
 			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery("(SELECT AuthorizationLevel FROM CustomerData WHERE EmailAddress = '" + username + "' AND Password = '" + password + "') UNION (SELECT AuthorizationLevel FROM EmployeeData WHERE EmailAddress = '" + username + "' AND Password = '" + password + "')");
+			ResultSet rs = st.executeQuery("(SELECT AuthorizationLevel, EmailAddress, Password FROM CustomerData WHERE EmailAddress = '" + username + "' AND Password = '" + password + "') UNION (SELECT AuthorizationLevel, EmailAddress, Password FROM EmployeeData WHERE EmailAddress = '" + username + "' AND Password = '" + password + "')");
 			while (rs.next()) {
 				authorizationLevel = rs.getString("AuthorizationLevel");
+				email= rs.getString("EmailAddress");
+				password= rs.getString("Password")
 			}
 		} catch(Exception e) {
 			System.out.println(e);
 		}
-		
 		Login login = new Login();
 		login.setRole(authorizationLevel);
+		login.setUserName(email);
+		login.setPassword(password);
 		if (authorizationLevel.equals("")) {
 			return null;
 		} else {
