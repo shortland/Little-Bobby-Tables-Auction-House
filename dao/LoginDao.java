7dff06b1c208be87b761f131632821b3c0ee4908
Josephine <Josephine@10.1.177.206>
2018-12-06 15:28:36 -0500

    Edit addUser()-David

--- dao/LoginDao.java
+++ dao/LoginDao.java
@@ -63,22 +63,23 @@ public class LoginDao {
 		// executeQuery() should be executeUpdate() for insert/updates
 
 
-		// String table;
-		// if ((login.getRole()).equals("customer")) {
-		// 	table = "CustomerData";
-		// }
-		// else {
-		// 	table = "EmployeeData";
-		// }
+		String table;
+		if ((login.getRole()).equals("customer")) {
+			table = "CustomerData";
+		}
+		else {
+			table = "EmployeeData";
+		}
 
-		// try {
-		// 	Class.forName("com.mysql.jdbc.Driver");
-		// 	Connection con = DriverManager.getConnection("jdbc:mysql://138.197.50.244:3306/LittleBobbyTablesAuctionHouse",  "littlebobbytables", "bestcse305group");
-		// 	Statement st = con.createStatement();
-		// 	ResultSet rs = st.executeQuery("INSERT INTO " + table + " (EmailAddress, Password) VALUES ('" + login.getUsername() + "', '" + login.getPassword() + "')");
-		// } catch(Exception e) {
-		// 	System.out.println(e);
-		// }
+		try {
+		 	Class.forName("com.mysql.jdbc.Driver");
+		 	Connection con = DriverManager.getConnection("jdbc:mysql://138.197.50.244:3306/LittleBobbyTablesAuctionHouse",  "littlebobbytables", "bestcse305group");
+		 	Statement st = con.createStatement();
+		 	ResultSet rs = st.executeQuery("INSERT INTO " + table + " (EmailAddress, Password) VALUES ('" + login.getUsername() + "', '" + login.getPassword() + "')");
+		 }catch(Exception e) {
+			 System.out.println(e);
+			 return "failure";
+		 }
 
 		return "success";
 	}
