70ced7ec3a7e354e7f5349d86f42c963f19d278e
shortland <ilankleiman@gmail.com>
2018-12-09 16:16:08 -0500

    manual merge of master

--- CSE305/src/main/java/dao/LoginDao.java
+++ CSE305/src/main/java/dao/LoginDao.java
@@ -30,7 +30,7 @@ public class LoginDao {
 			while (rs.next()) {
 				authorizationLevel = rs.getString("AuthorizationLevel");
 				email= rs.getString("EmailAddress");
-				pWord= rs.getString("Password")
+				pWord= rs.getString("Password");
 			}
 		} catch(Exception e) {
 			System.out.println(e);
@@ -63,23 +63,23 @@ public class LoginDao {
 		// executeQuery() should be executeUpdate() for insert/updates
 
 
-		String table;
-		if ((login.getRole()).equals("customer")) {
-			table = "CustomerData";
-		}
-		else {
-			table = "EmployeeData";
-		}
-
-		try {
-		 	Class.forName("com.mysql.jdbc.Driver");
-		 	Connection con = DriverManager.getConnection("jdbc:mysql://138.197.50.244:3306/LittleBobbyTablesAuctionHouse",  "littlebobbytables", "bestcse305group");
-		 	Statement st = con.createStatement();
-		 	ResultSet rs = st.executeQuery("INSERT INTO " + table + " (EmailAddress, Password) VALUES ('" + login.getUsername() + "', '" + login.getPassword() + "')");
-		 }catch(Exception e) {
-			 System.out.println(e);
-			 return "failure";
-		 }
+//		String table;
+//		if ((login.getRole()).equals("customer")) {
+//			table = "CustomerData";
+//		}
+//		else {
+//			table = "EmployeeData";
+//		}
+//
+//		try {
+//		 	Class.forName("com.mysql.jdbc.Driver");
+//		 	Connection con = DriverManager.getConnection("jdbc:mysql://138.197.50.244:3306/LittleBobbyTablesAuctionHouse",  "littlebobbytables", "bestcse305group");
+//		 	Statement st = con.createStatement();
+//		 	ResultSet rs = st.executeUpdate("INSERT INTO " + table + " (EmailAddress, Password) VALUES ('" + login.getUsername() + "', '" + login.getPassword() + "')");
+//		 }catch(Exception e) {
+//			 System.out.println(e);
+//			 return "failure";
+//		 }
 
 		return "success";
 	}
